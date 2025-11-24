c3b028435f6d4da488734f74a4084d583b857d34
ShanEior <18636758584@qq.com>
2025-11-24 19:01:45 +0800

    Site updated: 2025-11-24 19:01:44

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/11/23/First post/","2025/11/23/UKY周报二/","2025/11/23/UKY周报一/","2025/11/23/UKY周报四/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
